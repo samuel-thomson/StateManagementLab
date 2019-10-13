43badd4e72f021552fa98b4885dda5d4b541d6ac
Sam Thomson <samuelthomson62@gmail.com>
2019-10-13 10:41:42 -0400

    Second commit with project files

---
+++ StateManagementLab/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using StateManagementLab.Models;
+
+namespace StateManagementLab.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult Registration()
+        {
+            return View();
+        }
+        // The bottom Action is an Overload
+        public IActionResult Registration(User u)
+        {
+            List<User> Users = new List<User>();
+            User Pika = new User("Pikachu", "thundershock");
+            User Squirt = new User("Squirtle", "watergun");
+            User Bulb = new User("Bulb", "vinewhip");
+            User Charm = new User("Charmander", "flamethrower");
+            Users.Add(Charm);
+            Users.Add(Bulb);
+            Users.Add(Squirt);
+            Users.Add(Pika);
+            return View();
+        }
+
+    }
+}
---
+++ StateManagementLab/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StateManagementLab.Models
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int Age { get; set; }
+
+        public User(string username, string password) 
+        {
+            this.UserName = username;
+            this.Password = password;
+        }
+        public User()
+        {
+
+        }
+    }
+
+}
